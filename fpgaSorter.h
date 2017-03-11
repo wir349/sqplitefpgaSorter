66ae08fce336853e64ddfa7d1ef0df4471d123d8
Krittercon <krit098@hotmail.com>
2017-03-10 18:50:36 -0800

    Update fpgaSorter.h

--- fpgaSorter.h
+++ fpgaSorter.h
@@ -4,6 +4,7 @@
 *	Krit Sae Fang
 */
 #include <stdbool.h>
+#include <stdint.h>
 
 #ifndef FPGASORTER_FPGASORTER_H
 #define FPGASORTER_FPGASORTER_H "fpgaSorter.h"
@@ -11,124 +12,135 @@
 #endif //FPGASORTER_FPGASORTER_H
 
 // Constants
-#define ListSize 1000					// Assumed list size (Depends on available BRAM blocks)
-#define DataSize 10					// 80 bits for comparision data (10 characters)
-#define StructSize 12
+#define ListSize 1000                    // Assumed list size (Depends on available BRAM blocks)
+#define DataSize 10                    // 80 bits for comparision data (10 characters)
+#define StructSize (DataSize + sizeof(int8_t))
 
 // Constants
-#define InputBufferSize 400			// Used to save memory
-#define InputBufferMin 200				// Minimum in buffer before a refill
+#define InputBufferSize 400            // Used to save memory
+#define InputBufferMin 200                // Minimum in buffer before a refill
 
-	//---------------------------------------------------------------------------------------------
-	// Declarations and stuff
-	//---------------------------------------------------------------------------------------------
+//---------------------------------------------------------------------------------------------
+// Declarations and stuff
+//---------------------------------------------------------------------------------------------
 
-	// Struct declarations
-	typedef struct comparePair comparePair;	// Structured pair of ID and comparision data
+// Struct declarations
+typedef struct{
+    int8_t key;
+    char data[DataSize];
+} comparePair;    // Structured pair of ID and comparision data
 
-	// Struct for our ptr/compare value pair.
-	struct comparePair{
-		int key;				//ptr to actual object
-		char data[DataSize];	//Data to compare
-	};
+// Struct for our ptr/compare value pair.
+/*struct comparePair {
+    int key;                //ptr to actual object
+    char data[DataSize];    //Data to compare
+};*/
 
-	//---------------------------------------------------------------------------------------------
-	// "Public" functions and methods
-	//---------------------------------------------------------------------------------------------
-	
-	/*
-	*	Initializer
-	*	Prepares values for working with this sorter
-	*/
-	void fpgaSorterInitialize();
+//---------------------------------------------------------------------------------------------
+// "Public" functions and methods
+//---------------------------------------------------------------------------------------------
 
-	/*
-	*	Adds value to be linear sorted
-	*	Value is kept off BRAM until ready.
-	*/
-	void fpgaSorterInsert(
-		int key,
-		char *data,
-		int size
-	);
+/*
+*	Initializer
+*	Prepares values for working with this sorter
+*/
+void fpgaSorterInitialize();
 
-	/*
-	*	Quick reset
-	*	Returns counter to zero
-	*/
-	void fpgaSorterResetLinear();
-	
-	/*
-	*	Sorts the values in our working array
-	*	Does not yet return anything
-	*/
-	void fpgaSorterSortLinear();
-	
-	/*
-	*	Gets the sorted array
-	*	Copies the result array into the provided array
-	*	Not completely safe. Array needs to be large enough to accomadate
-	*	Uses the struct defined in this file
-	*	If not yet sorted, this does nothing.
-	*/
-	void fpgaSorterGetLinearResultArray(char *returnArray);
-	
-	/*
-	*	Gets a key from the sorted array. Returns 0 if not sorted
-	*/
-	int fpgaSorterGetLinearResultPos(int pos);
-	
-	/*
-	*	Gets a key from the sorted array. Returns 0 if not sorted or reached end
-	*	Returns values in sorted other each time it is called
-	*/
-	int fpgaSorterGetLinearResultNext();
-	
-	/*
-	*	Resets the read pointer to 0.
-	*/
-	void fpgaSorterResetLinearRead();
+/*
+*	Adds value to be linear sorted
+*	Value is kept off BRAM until ready.
+*/
+void fpgaSorterInsert(
+        int key,
+        char *data,
+        int size
+);
+
+/*
+*	Quick reset
+*	Returns counter to zero
+*/
+void fpgaSorterResetLinear();
+
+/*
+*	Sorts the values in our working array
+*	Does not yet return anything
+*/
+void fpgaSorterSortLinear();
+
+/*
+*	Gets the sorted array
+*	Copies the result array into the provided array
+*	Not completely safe. Array needs to be large enough to accomadate
+*	Uses the struct defined in this file
+*	If not yet sorted, this does nothing.
+*/
+void fpgaSorterGetLinearResultArray(comparePair *returnArray);
+
+/*
+*	Gets a key from the sorted array. Returns 0 if not sorted
+*/
+int fpgaSorterGetLinearResultPos(int pos);
+
+/*
+*	Gets a key from the sorted array. Returns 0 if not sorted or reached end
+*	Returns values in sorted other each time it is called
+*/
+int fpgaSorterGetLinearResultNext();
+
+/*
+*	Resets the read pointer to 0.
+*/
+void fpgaSorterResetLinearRead();
 
 //---------------------------------------------------------------------------------------------
 // "Private" functions and methods. Or static in this case...
 //---------------------------------------------------------------------------------------------
 
-	/*
-	*	Main sorting algorithm
-	*	Maximum value that can be sorted is determined by constant ListSize
-	*	Sorted array overwrites given array
-	*/
-	static void hlsLinearSort(
-		comparePair *p,
-		int pSize
-	);
+/*
+*	Main sorting algorithm
+*	Maximum value that can be sorted is determined by constant ListSize
+*	Sorted array overwrites given array
+*/
+void hlsLinearSort(
+        comparePair *p,
+        int pSize
+);
 
-	/*
-	*	Helper function for vopying arrays.
-	*/
-	//static copyArray(comparePair source, comparePair destination, int size){
-	//	memcpy(source, destination, size);
-	//}
+/*
+*	Helper function for vopying arrays.
+*/
+void copyArray(char * source, int sourceOffset, char * destination, int destOffset, int size);
 
-	/*
-	*	Helper function for comparing values in the struct
-	*	Bitwise, byte by byte via char
-	*/
-	static bool dataCompare(comparePair x, comparePair y);
+/*
+*	Helper function for vopying arrays.
+*/
+void copyArrayComparePair(comparePair * source, int sourceOffset, comparePair * destination, int destOffset, int size);
 
-	/*
-	*	Helper function for checking for empty cell.
-	*	Makes life easier since I don't have to do multiple nests after nests
-	*/
-	static bool arrayCellIsEmpty(int i, int j);
+void copyArrayBorder(void * source, int sourceOffset, void * destination, int destOffset, int size);
 
-	/*
-	*	Merge sort implementation for FPGAs
-	*/
-	static void hlsMergeSort(
-		char *p1,
-		int p1Size,
-		char *p2,
-		int p2Size,
-		char *pFinal
-	);
+void copyArrayOuter(void * source, int sourceOffset, void * destination, int destOffset, int size);
+
+/*
+*	Helper function for comparing values in the struct
+*	Bitwise, byte by byte via char
+*/
+bool dataCompare(comparePair x, comparePair y);
+
+/*
+*	Helper function for checking for empty cell.
+*	Makes life easier since I don't have to do multiple nests after nests
+*/
+bool arrayCellIsEmpty(int i, int j);
+
+/*
+*	Merge sort implementation for FPGAs
+*/
+/*
+static void hlsMergeSort(
+        char *p1,
+        int p1Size,
+        char *p2,
+        int p2Size,
+        char *pFinal
+);*/
