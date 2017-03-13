61c1a66ec0f51d0d8a422fee8b802f800e67fc9d
Krittercon <krit098@hotmail.com>
2017-03-12 23:03:42 -0700

    Update fpgaSorter.c

--- fpgaSorter.c
+++ fpgaSorter.c
@@ -1,152 +1,151 @@
-/*
-*	CS259 Project - Linear Shift + Merge Sort for FPGAs
-*	UCLA WI2017
-*	Krit Sae Fang
-*/
+//
+// Created by Krittercon on 3/12/2017.
+//
 #include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include "fpgaSorter.h"
 
-#ifndef FPGASORTER_FPGASORTER_H
-#define FPGASORTER_FPGASORTER_H "fpgaSorter.h"
-
-#endif //FPGASORTER_FPGASORTER_H
-
-// Constants
-#define ListSize 500                   // Assumed list size (Depends on available BRAM blocks)
-#define PaddingData sizeof(int8_t)
-#define KeySize sizeof(int16_t)
-#define DataSize sizeof(int64_t)
-#define StructSize (DataSize + KeySize + PaddingData)
-
-// Constants
-#define InputBufferSize 400            // Used to save memory
-#define InputBufferMin 200                // Minimum in buffer before a refill
-
-//---------------------------------------------------------------------------------------------
-// Declarations and stuff
-//---------------------------------------------------------------------------------------------
-
-// Struct declarations
-typedef struct{
-    int8_t paddingData3;
-    int16_t key;
-    int64_t data;
-    //int32_t paddingData;
-    //int16_t paddingData2;
-    //int8_t paddingData3;
-} comparePair;    // Structured pair of ID and comparision data
-
-// Struct for our ptr/compare value pair.
-/*struct comparePair {
-    int key;                //ptr to actual object
-    char data[DataSize];    //Data to compare
-};*/
-
-//---------------------------------------------------------------------------------------------
-// "Public" functions and methods
-//---------------------------------------------------------------------------------------------
+bool initialized;
+bool isSorted;
+comparePair workingArrayLinear[ListSize];
+int workingArrayLinearSize;
+int workingArrayLinearReadPos;
 
 /*
-*	Initializer
-*	Prepares values for working with this sorter
+ *  Initializer
 */
-void fpgaSorterInitialize();
+void fpgaSorterInitialize(){
+    if(!initialized){
+        isSorted = false;
+        workingArrayLinearSize = 0;
+        workingArrayLinearReadPos = 0;
+
+        initialized = true;
+    }
+}
 
 /*
-*	Adds value to be linear sorted
-*	Value is kept off BRAM until ready.
-*/
-void fpgaSorterInsert(
-        int key,
-        char *data,
-        int size
-);
+ *  Adds an entry
+ */
+void fpgaSorterInsert(int key, char * data, int size){
+    if(workingArrayLinearSize > ListSize){
+        return;
+    }
+
+    if(!initialized){
+        fpgaSorterInitialize();
+    }
+
+    //DEBUG
+    key = workingArrayLinearSize;
+
+    workingArrayLinear[workingArrayLinearSize].key = key;
+    memcpy(&workingArrayLinear[workingArrayLinearSize].data, data, DataSize);
+
+    //DEBUG
+    workingArrayLinear[workingArrayLinearSize].data = rand();
+    printf("\nAdding key %i with value %i", key, workingArrayLinear[workingArrayLinearSize].data);
+
+    workingArrayLinearSize++;
+}
+
+void fpgaSorterSortLinear() {
+    if(!isSorted){
+        hlsLinearSort(workingArrayLinear, workingArrayLinearSize);
+        isSorted = true;
+    }
+}
+
+int fpgaSorterGetLinearResultNext(){
+    if(isSorted && (workingArrayLinearReadPos < workingArrayLinearSize)) {
+        workingArrayLinearReadPos++;
+
+        //DEBUG
+        printf("\nReturning key %i with value %i", workingArrayLinear[workingArrayLinearReadPos - 1].key, workingArrayLinear[workingArrayLinearReadPos - 1].data);
+
+        return workingArrayLinear[workingArrayLinearReadPos - 1].key;
+    }
+
+    return 0;
+}
+
+void hlsLinearSort(comparePair * workingArray, int16_t inputSize){
+#pragma HLS INTERFACE m_axi port=workingArray bundle=gmem
+#pragma HLS INTERFACE s_axilite port=workingArray bundle=control
+#pragma HLS INTERFACE s_axilite port=inputsize bundle=control
+#pragma HLS INTERFACE s_axilite port=return bundle=control
+
+    // Localized arrays
+    comparePair localInputArray[ListSize];
+    comparePair localFinalArray[ListSize];
+#pragma HLS ARRAY_PARTITION variable=localFinalArray dim=1
+
+    // Copy into local array
+    memcpy(localInputArray, workingArray, (inputSize * ComparePairSize));
+
+    // Handle first value
+    localFinalArray[0] = localInputArray[0];
+
+    for (int16_t inputNumber = 1; inputNumber < inputSize; inputNumber++){
+        // Loop through the input array and read each value to insert into the sorted list
+
+        // Put our working value into a register
+        comparePair inputPair = localInputArray[inputNumber];
+
+        for(int16_t finalListSlot = (ListSize-1); finalListSlot >= 0; finalListSlot--){
+#pragma HLS UNROLL
+            // Parallel read of all values in the finalized list
+
+            // Put all working values into registers
+            comparePair finalSlotValueAbove;
+            if (finalListSlot == 0){
+                finalSlotValueAbove.data = 0;
+            } else {
+                finalSlotValueAbove = localFinalArray[finalListSlot - 1];
+            }
+            comparePair finalSlotValue = localFinalArray[finalListSlot];
+            comparePair finalSlotValueNew = finalSlotValue;
+
+            if(hlsArrayCellIsEmpty(finalListSlot, inputNumber) && (!hlsArrayCellIsEmpty((finalListSlot - 1), inputNumber))){
+                // This cell is empty, above is not
+                if(inputPair.data > finalSlotValueAbove.data){
+                    // Input value is larger than above value. This goes here
+                    finalSlotValueNew = inputPair;
+                } else {
+                    // input value is smaller than above value. This goes earlier in the list and we receive cascade
+                    finalSlotValueNew = finalSlotValueAbove;
+                }
+
+            } else if(!hlsArrayCellIsEmpty(finalListSlot, inputNumber)) {
+                // This cell is filled
+                if(inputPair.data < finalSlotValueAbove.data){
+                    // Input value is smaller than above value. This goes earlier in the list and we receive cascade
+                    finalSlotValueNew = finalSlotValueAbove;
+                } else if(inputPair.data < finalSlotValue.data) {
+                    // input value is larger than above value but smaller than ours. This goes here
+                    finalSlotValueNew = inputPair;
+                }
+            }
+
+            // Update this slow with new value
+            localFinalArray[finalListSlot] = finalSlotValueNew;
+        }
+
+    }
+
+    // Copy back into passed in array
+    memcpy(workingArray, localFinalArray, (inputSize * ComparePairSize));
+}
 
 /*
-*	Quick reset
-*	Returns counter to zero
-*/
-void fpgaSorterResetLinear();
-
-/*
-*	Sorts the values in our working array
-*	Does not yet return anything
-*/
-void fpgaSorterSortLinear();
-
-/*
-*	Gets the sorted array
-*	Copies the result array into the provided array
-*	Not completely safe. Array needs to be large enough to accomadate
-*	Uses the struct defined in this file
-*	If not yet sorted, this does nothing.
-*/
-void fpgaSorterGetLinearResultArray(comparePair *returnArray);
-
-/*
-*	Gets a key from the sorted array. Returns 0 if not sorted
-*/
-int fpgaSorterGetLinearResultPos(int pos);
-
-/*
-*	Gets a key from the sorted array. Returns 0 if not sorted or reached end
-*	Returns values in sorted other each time it is called
-*/
-int fpgaSorterGetLinearResultNext();
-
-/*
-*	Resets the read pointer to 0.
-*/
-void fpgaSorterResetLinearRead();
-
-//---------------------------------------------------------------------------------------------
-// "Private" functions and methods. Or static in this case...
-//---------------------------------------------------------------------------------------------
-
-/*
-*	Main sorting algorithm
-*	Maximum value that can be sorted is determined by constant ListSize
-*	Sorted array overwrites given array
-*/
-void hlsLinearSort(
-        comparePair *p,
-        uint16_t pSize
-);
-
-/*
-*	Helper function for vopying arrays.
-*/
-void copyArray(char * source, uint8_t sourceOffset, char * destination, uint8_t destOffset, uint16_t size);
-
-/*
-*	Helper function for vopying arrays.
-*/
-void copyArrayComparePair(comparePair * source, uint8_t sourceOffset, comparePair * destination, uint8_t destOffset, uint16_t size);
-
-void copyArrayBorder(void * source, int sourceOffset, void * destination, int destOffset, int size);
-
-void copyArrayOuter(void * source, int sourceOffset, void * destination, int destOffset, int size);
-
-/*
-*	Helper function for comparing values in the struct
-*	Bitwise, byte by byte via char
-*/
-bool dataCompare(int64_t a, int64_t b);
-
-/*
-*	Helper function for checking for empty cell.
-*	Makes life easier since I don't have to do multiple nests after nests
-*/
-bool arrayCellIsEmpty(int i, int j);
-
-/*
-*	Merge sort implementation for FPGAs
-*/
-/*
-static void hlsMergeSort(
-        char *p1,
-        int p1Size,
-        char *p2,
-        int p2Size,
-        char *pFinal
-);*/
+ *  Quick checking function for empty slot
+ */
+bool hlsArrayCellIsEmpty(int16_t curSlot, int16_t inputPos){
+    if(curSlot < 0){
+        return false;
+    }
+    return (curSlot >= inputPos);
+}
