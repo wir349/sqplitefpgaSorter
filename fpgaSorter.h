703ebaf7bee5779e60c7696c71831a15ac657ec6
Krittercon <krit098@hotmail.com>
2017-03-19 17:00:41 -0700

    Update fpgaSorter.h

--- fpgaSorter.h
+++ fpgaSorter.h
@@ -8,27 +8,27 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define ListSize 10
+#define ListSize 400
 
-#define PaddingSize sizeof(int8_t)
-#define KeySize sizeof(int16_t)
-#define DataSize sizeof(int64_t)
-#define ComparePairSize (PaddingSize + KeySize + DataSize)
+#define KeySize sizeof(int)
+#define DataSize sizeof(int)
+#define ComparePairSize (KeySize + DataSize)
 
 typedef struct{
-    int8_t paddingData;
-    int16_t key;
-    int64_t data;
+    int key;
+    int data;
 } comparePair;
 
 //----------------------------------------------------------------
 //  Forward declarations
 //----------------------------------------------------------------
+void merge_up(int *arr, int n);
+void merge_down(int *arr, int n);
+void do_sort(int * arr, int n);
 void fpgaSorterInitialize();
 void fpgaSorterInsert(int key, char * data, int size);
 void fpgaSorterSortLinear();
 int fpgaSorterGetLinearResultNext();
-void hlsLinearSort(comparePair * workingArray, int16_t inputSize);
-bool hlsArrayCellIsEmpty(int16_t curSlot, int16_t inputPos);
+void hlsLinearSort(comparePair * workingArray, int inputSize);
 
 #endif //SQLPROJECT_FPGASORTER_H
