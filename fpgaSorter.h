ef22baf1bb3467a4844bf4c95017a4f51a89790d
Krittercon <krit098@hotmail.com>
2017-03-21 17:38:19 -0700

    Updated for buffered input

--- fpgaSorter.h
+++ fpgaSorter.h
@@ -1,14 +1,10 @@
-//
-// Created by Krittercon on 3/12/2017.
-//
-
 #ifndef SQLPROJECT_FPGASORTER_H
 #define SQLPROJECT_FPGASORTER_H
 
 #include <stdint.h>
 #include <stdbool.h>
 
-#define ListSize 500
+#define ListSize 400
 
 #define KeySize sizeof(int16_t)
 #define DataSize sizeof(int64_t)
@@ -22,11 +18,12 @@ typedef struct{
 //----------------------------------------------------------------
 //  Forward declarations
 //----------------------------------------------------------------
+void merge_up(comparePair *arr, int16_t n);
+void merge_down(comparePair *arr, int16_t n);
 void fpgaSorterInitialize();
 void fpgaSorterInsert(int key, char * data, int size);
 void fpgaSorterSortLinear();
 int fpgaSorterGetLinearResultNext();
 void hlsLinearSort(comparePair * workingArray, int16_t inputSize);
-bool hlsArrayCellIsEmpty(int16_t curSlot, int16_t inputPos);
 
 #endif //SQLPROJECT_FPGASORTER_H
